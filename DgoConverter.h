06fda52562c3f150368c37b402555598b6830986
RUTE-FSMUSEUM <contact@fsmuseum.jpn.org>
2022-11-16 10:00:09 +0900

    Fix KeyTrigger struct

--- DgoConverter.h
+++ DgoConverter.h
@@ -10,6 +10,8 @@
 #include <dinputd.h>
 
 const int NAME_SIZE = 128;
+const int NUM_BUTTONS = 10;
+const WCHAR* NOT_IN_USE = L"N/A";
 
 typedef struct KeyConfig {
 	WORD EB;
@@ -32,49 +34,36 @@ typedef struct KeyInfo {
 	KEYCONFIG KEY;
 }KEYINFO;
 
-typedef struct TriggerCondition {
-	WORD EB;
-	WORD B8;
-	WORD B7;
-	WORD B6;
-	WORD B5;
-	WORD B4;
-	WORD B3;
-	WORD B2;
-	WORD B1;
-	WORD NT;
-	WORD P1;
-	WORD P2;
-	WORD P3;
-	WORD P4;
-	WORD P5;
-	WORD A;
-	WORD B;
-	WORD C;
-	WORD START;
-	WORD SELECT;
-}TRIGCOND;
-
-typedef struct ButtonForMascon {
-	bool BT0;
-	bool BT1;
-	bool BT2;
-	bool BT3;
-	bool BT4;
-	bool BT5;
-	bool BT6;
-	bool BT7;
-	bool BT8;
-	bool BT9;
-}ISMASCONBT;
+typedef struct WcharTriggerCondition {
+	WCHAR EB[NAME_SIZE];
+	WCHAR B8[NAME_SIZE];
+	WCHAR B7[NAME_SIZE];
+	WCHAR B6[NAME_SIZE];
+	WCHAR B5[NAME_SIZE];
+	WCHAR B4[NAME_SIZE];
+	WCHAR B3[NAME_SIZE];
+	WCHAR B2[NAME_SIZE];
+	WCHAR B1[NAME_SIZE];
+	WCHAR NT[NAME_SIZE];
+	WCHAR P1[NAME_SIZE];
+	WCHAR P2[NAME_SIZE];
+	WCHAR P3[NAME_SIZE];
+	WCHAR P4[NAME_SIZE];
+	WCHAR P5[NAME_SIZE];
+	WCHAR A[NAME_SIZE];
+	WCHAR B[NAME_SIZE];
+	WCHAR C[NAME_SIZE];
+	WCHAR START[NAME_SIZE];
+	WCHAR SELECT[NAME_SIZE];
+}TRIGCOND_WCHAR;
 
 typedef struct KeyTriggerInfo {
 	WCHAR NAME[NAME_SIZE];
-	TRIGCOND AX_X;
-	TRIGCOND AX_Y;
-	TRIGCOND AX_Z;
-	TRIGCOND BT;
-	ISMASCONBT ISMASCON; // マスコンの位置がAXISではなくボタンでも表現されるJC-PS101Uのためだけに作ったconfigで、もっと良い方法はないのか模索中
+	TRIGCOND_WCHAR AX_X;
+	TRIGCOND_WCHAR AX_Y;
+	TRIGCOND_WCHAR AX_Z;
+	TRIGCOND_WCHAR BT;
+	bool isExclusiveButton[NUM_BUTTONS]; // マスコンの位置がAXISではなくボタンでも表現されるJC-PS101Uのためだけに作ったconfigで、もっと良い方法はないのか模索中
 }KEYTRIGGERINFO;
 
 typedef struct ButtonState {
